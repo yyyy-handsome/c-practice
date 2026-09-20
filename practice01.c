c5c3885aa04aa9bcacabbe9d4c5aab9bc9f375e1
yyyy <3441877957@qq.com>
2026-09-20 23:12:30 +0800

    完成 ractic02 打印三种三角形

---
+++ practice01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("yyyy-handsome!\n");
+    printf("学号787878\n");
+    printf("专业计算机科学与技术\n");    
+    return 0;
+}
---
+++ practice02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("     8\n");
+    printf("    88\n");
+    printf("   888\n");
+    printf("  8888\n");
+    printf(" 88888\n");
+    printf("\n");
+    printf("8\n");
+    printf("88\n");
+    printf("888\n");
+    printf("8888\n");
+    printf("88888\n");
+    printf("\n");
+    printf("88888\n");
+    printf("8888\n");
+    printf("888\n");
+    printf("88\n");
+    printf("8\n");
+
+    return 0;
+}
