b2e4976e037bdedc7b855920183d3709c1fe21fa
yyyy <3441877957@qq.com>
2026-09-21 23:16:02 +0800

    完成 practice03:变量 占位符 类型转换

--- practice03.c
+++ practice03.c
@@ -15,5 +15,13 @@ int main(void){
     int score = 90;
     score = score + 5;
     printf("score=%d\n",score);
+    printf("\n");
+    double r = 2.5;
+    double area = 3.14159*r*r;
+    printf("半径为%.2f的圆的面积为%.2f\n",r, area);
+    printf("\n");
+    int r2 = 2.5;
+    printf("r2=%d\n",r2);
+    printf("圆的面积为%.2f\n",3.14159*r2*r2);
      return 0;
 }
